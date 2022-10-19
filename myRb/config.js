e5d2c74a1cf895031c51cd655300d30824b84999
Radu <55529132+raduspa@users.noreply.github.com>
2022-10-19 12:45:12 +0300

    Update config.js

--- myRb/config.js
+++ myRb/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "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"
+  token: "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",
   platform: 'github',
   logLevel: 'debug',
   onboardingConfig: {
