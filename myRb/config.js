577a62df4f31817857eebfb4a4a8fa8af43035a8
Radu <55529132+raduspa@users.noreply.github.com>
2022-10-19 13:14:50 +0300

    Update config.js

--- myRb/config.js
+++ myRb/config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  token: "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",
-  platform: 'github',
   logLevel: 'debug',
   onboardingConfig: {
     extends: ['config:base'],
   },
-  repositories: ['raduspa/renovateTest'],
-  renovateFork: true,
+  repositories: ['raduspa/renovateTest'],  
   gitAuthor: "rBonWork <rbb@worker.wo>",
   username: "julienp",
   onboarding: false,
