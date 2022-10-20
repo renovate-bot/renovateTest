f6209eaa008f608d8751dd7cb28f7462e9278975
Radu <55529132+raduspa@users.noreply.github.com>
2022-10-20 21:59:31 +0300

    Update config.js

--- myRb/config.js
+++ myRb/config.js
@@ -1,52 +1,7 @@
-let githubToken = process.env.SECRET_TOKEN
-module.exports = {
-  platform: 'github',
-  logLevel: 'debug',
-  onboardingConfig: {
-    extends: ['config:base'],
-  },
-  repositories: ['raduspa/renovateTest'],
-  enabledManagers: [ "gradle", "npm" ],
-  renovateFork: true,
-  gitAuthor: "Julien <julien@caffeine.lu>",
-  username: "julienp",
-  onboarding: false,
-  requireConfig: false,
-  printConfig: true,
-  
-    "lockFileMaintenance": {
-    "enabled": true
-  },
-  "baseBranches": [
-    "main"
-  ],
-  "ignorePaths": [
-    "/node_modules/",
-    "/test/",
-    "/tests/"
-  ],
-  "packageRules": [
-    {
-      "description": "explicitly enable npm groupname packages public version upgrades",
-      "matchManagers": [
-        "npm"
-      ],
-      "matchCurrentVersion": "!/.*-.*/",
-      "enabled": true
-    },
-    {
-      "description": "explicitly enable maven groupname packages",
-      "matchManagers": [
-        "gradle"
-      ],      
-      "enabled": true
-    },
-    {
-      "description": "Disable for global.json",
-      "matchFiles": [
-        "global.json"
-      ],
-      "enabled": false
-    }
+{
+  "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+  "extends": [
+    "config:base",
+    ":disableDependencyDashboard"
   ]
-};
+}
